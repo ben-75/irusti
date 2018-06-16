7da7af0c0f74d23000841a80aa909b4cf596197a
bmangez <Benoit.Mangez@mcl-technologies.com>
2018-06-16 17:41:08 +0200

    tests

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,4 +1,9 @@
 use converter::to_bytes;
+use std::hash::{Hash, self};
+use std::fmt;
+use std::str::FromStr;
+use converter::i8_to_trits;
+use converter::tuple_2_char;
 
 const SIZE :usize = 1604;
 const TAG_SIZE_IN_BYTES :usize = 17; // = ceil(81 TRITS / 5 TRITS_PER_BYTE)
@@ -67,5 +72,148 @@ impl Transaction {
             if self.arr[i]!=0 {return false;}
         }
         self.arr[1367]<5 && self.arr[1367]>-5
+    }
+}
+
+impl PartialEq for Transaction {
+    fn eq(&self, other: &Transaction) -> bool {
+        for i in 0..SIZE {
+            if self.arr[i]!=other.arr[i] {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl Eq for Transaction {}
+
+impl fmt::Debug for Transaction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Transaction-Trytes: {}", self.to_string())
+    }
+}
+
+impl ToString for Transaction{
+
+    fn to_string(&self) -> String {
+        let mut tryte_count = TRINARY_SIZE;
+        let mut response:String = "".to_string();
+        let mut remaining_count = 0;
+        let mut b0 :i8 = 0;
+        let mut b1 :i8 = 0;
+        for byte_index in 0..SIZE {
+            if tryte_count == 0 {break;}
+            let [t0,t1,t2,t3,t4] = i8_to_trits(self.arr[byte_index]);
+            match remaining_count {
+                0 => {
+                    response.push(tuple_2_char((t0, t1, t2)));
+                    tryte_count -=1;
+                    //(b0,b1) =(t3,t4);
+                    b0 = t3;
+                    b1 = t4;
+                    remaining_count = 2;
+                }
+                1 => {
+                    response.push(tuple_2_char((b0, t0, t1)));
+                    tryte_count -=1;
+                    if tryte_count == 0 {break;}
+                    response.push(tuple_2_char((t2, t3, t4)));
+                    tryte_count -=1;
+                    remaining_count = 0;
+                }
+                2 => {
+                    response.push(tuple_2_char((b0, b1, t0)));
+                    tryte_count -=1;
+                    if tryte_count == 0 {break;}
+                    response.push(tuple_2_char((t1, t2, t3)));
+                    tryte_count -=1;
+                    b0 = t4;
+                    remaining_count = 1;
+                }
+                _ => panic!("cannot append. remainig count = {}",remaining_count),
+
+            }
+        }
+        response
+    }
+}
+
+impl FromStr for Transaction {
+    type Err = ();
+
+    fn from_str(trytes: &str) -> Result<Transaction, ()> {
+        let mut response: [i8;SIZE] = [0;SIZE];
+
+        let mut index_in_byte = 0;
+        let mut byte_index = 0;
+        for c in trytes.to_string().chars() {
+            match c {
+                '9' => add_trits((0,0,0),byte_index,index_in_byte,&mut response),
+                'A' => add_trits((1,0,0),byte_index,index_in_byte,&mut response),
+                'B' => add_trits((-1,1,0),byte_index,index_in_byte,&mut response),
+                'C' => add_trits((0,1,0),byte_index,index_in_byte,&mut response),
+                'D' => add_trits((1,1,0),byte_index,index_in_byte,&mut response),
+                'E' => add_trits((-1,-1,1),byte_index,index_in_byte,&mut response),
+                'F' => add_trits((0,-1,1),byte_index,index_in_byte,&mut response),
+                'G' => add_trits((1,-1,1),byte_index,index_in_byte,&mut response),
+                'H' => add_trits((-1,0,1),byte_index,index_in_byte,&mut response),
+                'I' => add_trits((0,0,1),byte_index,index_in_byte,&mut response),
+                'J' => add_trits((1,0,1),byte_index,index_in_byte,&mut response),
+                'K' => add_trits((-1,1,1),byte_index,index_in_byte,&mut response),
+                'L' => add_trits((0,1,1),byte_index,index_in_byte,&mut response),
+                'M' => add_trits((1,1,1),byte_index,index_in_byte,&mut response),
+                'N' => add_trits((-1,-1,-1),byte_index,index_in_byte,&mut response),
+                'O' => add_trits((0,-1,-1),byte_index,index_in_byte,&mut response),
+                'P' => add_trits((1,-1,-1),byte_index,index_in_byte,&mut response),
+                'Q' => add_trits((-1,0,-1),byte_index,index_in_byte,&mut response),
+                'R' => add_trits((0,0,-1),byte_index,index_in_byte,&mut response),
+                'S' => add_trits((1,0,-1),byte_index,index_in_byte,&mut response),
+                'T' => add_trits((-1,1,-1),byte_index,index_in_byte,&mut response),
+                'U' => add_trits((0,1,-1),byte_index,index_in_byte,&mut response),
+                'V' => add_trits((1,1,-1),byte_index,index_in_byte,&mut response),
+                'W' => add_trits((-1,-1,0),byte_index,index_in_byte,&mut response),
+                'X' => add_trits((0,-1,0),byte_index,index_in_byte,&mut response),
+                'Y' => add_trits((1,-1,0),byte_index,index_in_byte,&mut response),
+                'Z' => add_trits((-1,0,0),byte_index,index_in_byte,&mut response),
+                _ => return Err(()),
+            }
+            if index_in_byte > 1 {byte_index +=1;}
+            index_in_byte = (index_in_byte+3)%5;
+        }
+        Ok(Transaction{arr:response})
+    }
+
+}
+
+
+fn add_trits((t0,t1,t2) :(i8,i8,i8),byte_index :usize, index_in_byte :u32, byte_array :&mut [i8;SIZE])->(){
+    let factor = 3_i8.pow(index_in_byte);
+    if index_in_byte<=2 {
+        byte_array[byte_index] += t0 * factor + t1 * 3 * factor + t2 * 9 *factor;
+    }
+    if index_in_byte==3 {
+        byte_array[byte_index] += t0 * factor + t1 * 3 * factor;
+        byte_array[byte_index+1] = t2;
+    }
+    if index_in_byte==4 {
+        byte_array[byte_index] += t0 * factor;
+        byte_array[byte_index+1] = t1+ 3*t2;
+    }
+    ()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new(){
+        let tx = Transaction::new("A".as_ref()).unwrap();
+        let mut array = [0_i8;SIZE];
+        array[0] = 1_i8;
+        let tx2 = Transaction{arr: array};
+        assert_eq!(tx,tx2);
     }
 }
